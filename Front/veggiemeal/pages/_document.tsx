0daa3e19a577eaec3cee26d7d5f8faa8c724b0bf
gimewn <yoonju_1120@naver.com>
2022-09-21 12:06:37 +0900

    [S07P22C205-118][FE-윤주]feat:meta 위치 변경, font 방식  변경

--- Front/veggiemeal/pages/_document.tsx
+++ Front/veggiemeal/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
       {/* 웹,카카오,네이버 */}
       <meta property="og:title" content="베지밀" />
       <meta property="og:description" content="더 건강하고 더 경제적인 당신의 식사를 위해" />
@@ -15,6 +15,8 @@ export default function Document() {
       <meta name="twitter:title" content="베지밀" />
       <meta name="twitter:description" content="더 건강하고 더 경제적인 당신의 식사를 위해" />
       <meta name="twitter:image" content="https://cdn.pixabay.com/photo/2019/08/19/07/45/corgi-4415649_960_720.jpg" />
+      <link href="https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
