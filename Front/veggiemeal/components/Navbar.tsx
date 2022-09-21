048327d2eedd4002625861be6dc42337fdef8503
gimewn <yoonju_1120@naver.com>
2022-09-21 15:44:41 +0900

    [S07P22C205-118][FE-윤주]feat:bootstrap 설정 변경

--- Front/veggiemeal/components/Navbar.tsx
+++ Front/veggiemeal/components/Navbar.tsx
@@ -8,8 +8,8 @@ import { useRouter } from 'next/router';
 function BasicExample() {
   const router = useRouter();
   return (
-    <Navbar expand="sm" fixed="top">
-      <Container id={styles.navigator}>
+    <Navbar expand="sm" fixed="top" id={styles.navigator}>
+      <Container>
         <Navbar.Brand href="/" className="d-flex align-items-center">
           <Image src="/Logo.png" alt="베지밀의 로고, 그릇에서 새싹이 발아하는 모습을 형상화했다."
           width={110} height={40} />
@@ -17,10 +17,10 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto" id={styles.menus}>
-            <Nav.Link href="/" className={router.pathname == "/" ? "now" : "" }>홈</Nav.Link>
-            <Nav.Link href="/recipe" className={router.pathname == "/recipe" ? "now" : "" }>레시피</Nav.Link>
-            <Nav.Link href="/cart" className={router.pathname == "/cart" ? "now" : "" }>장바구니</Nav.Link>
-            <Nav.Link href="/prices" className={router.pathname == "/prices" ? "now" : "" }>물가분석</Nav.Link>
+            <Nav.Link href="/" className={router.pathname == "/" ? "nav_active" : "" }>홈</Nav.Link>
+            <Nav.Link href="/recipe" className={router.pathname == "/recipe" ? "nav_active" : "" }>레시피</Nav.Link>
+            <Nav.Link href="/cart" className={router.pathname == "/cart" ? "nav_active" : "" }>장바구니</Nav.Link>
+            <Nav.Link href="/prices" className={router.pathname == "/prices" ? "nav_active" : "" }>물가분석</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
