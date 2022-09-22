72acad1c409521f4bd84efe4412c875a7c639403
gimewn <yoonju_1120@naver.com>
2022-09-22 15:53:13 +0900

    [S07P22C205-151][FE-윤주]feat:냉장고 레시피 웹 퍼블리싱 ing

--- Front/veggiemeal/pages/recipe/fridge/index.tsx
+++ Front/veggiemeal/pages/recipe/fridge/index.tsx
@@ -10,9 +10,14 @@ export default function Fridge() {
       </Head>
       <main>
         <header className={styles.header}>
-          <Image src="/cooking.png" width={150} height={150} />
-          <h1>냉장고를 부탁해</h1>
+          <Image src="/cooking.png" width={50} height={50} />
+          <h1 className={styles.main_title}>냉장고를 부탁해</h1>
         </header>
+        <section className={styles.main_intro}>
+          <p>지금 냉장고 속에 있는 재료나, 원하는 재료를 선택해주세요! </p>
+          <p>그에 맞는 레시피를 보여드릴게요. </p>
+        </section>
+        <section className={styles.choice}></section>
       </main>
     </>
   )
