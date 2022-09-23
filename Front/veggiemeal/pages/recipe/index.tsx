e142f4f53c8bc20c38ab3fbfb580099b31feb2f6
유이서 <yoojh456@naver.com>
2022-09-23 13:03:53 +0900

    [S07P22C205-139][FE-이서]feat:레시피 인덱스 재수정 및 채식 레시피 모바일 퍼블리싱

--- Front/veggiemeal/pages/recipe/index.tsx
+++ Front/veggiemeal/pages/recipe/index.tsx
@@ -3,11 +3,15 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import styles from 'styles/Recipe.module.scss';
 
-import Button from 'react-bootstrap/Button';
 import leafy from '/public/leafy_green.png';
 import cooking from '/public/cooking.png';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+
+
 
 export default function Recipe() {
   const router = useRouter();
@@ -21,25 +25,27 @@ export default function Recipe() {
         <header>
           <h1 className={styles.recipe_title}>레시피 모아 보기</h1>
         </header>
-        <section className={styles.recipe_section}>
-          <Row>
-            <Col sm={6}>
-              <Button className='btn' onClick={() => router.push('/recipe/veggie')}>
-                <span>채식 단계에</span>
-                <span>따른 레시피를 보여드려요</span>
-                <Image className={styles.leafy} src={leafy} alt="leafy_green" quality={100} />
-                <p>채식</p>
-              </Button>
-            </Col>
-            <Col sm={6}>
-              <Button className="btn" onClick={() => router.push('/recipe/fridge')}>
-                <span>원하는 재료에</span>
-                <span>따른 레시피를 보여드려요</span>
-                <Image className={styles.cooking} src={cooking} alt="cooking pan" quality={100} />
-                <p>냉장고</p>
-              </Button>
-            </Col>
-          </Row>
+        <section>
+          <Container>
+            <Row className='justify-content-sm-center;align-item-sm-center'>
+              <Col>
+                <Button id={styles.veggie_btn} onClick={() => router.push('/recipe/veggie')}>
+                  <p>채식 단계에</p>
+                  <p>따른 레시피를 보여드려요</p>
+                  <Image className={styles.leafy} src={leafy} alt="leafy_green" quality={100} width={70} height={70} />
+                  <p>채식</p>
+                </Button>
+              </Col>
+              <Col>
+                <Button id={styles.fridge_btn} onClick={() => router.push('/recipe/fridge')}>
+                  <p>원하는 재료에</p>
+                  <p>따른 레시피를 보여드려요</p>
+                  <Image className={styles.cooking} src={cooking} alt="cooking pan" quality={100} width={70} height={70} />
+                  <p>냉장고</p>
+                </Button>
+              </Col>
+            </Row>
+          </Container>
         </section>
       </main>
     </div >
--- Front/veggiemeal/pages/recipe/veggie/index.tsx
+++ Front/veggiemeal/pages/recipe/veggie/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Image from 'next/image';
 import leafy from '/public/leafy_green.png';
-import styles from 'styles/veggie.module.scss'
+import styles from 'styles/Veggie.module.scss';
+import Form from 'react-bootstrap/Form';
+import egg from '/public/egg.png';
+import fish from '/public/fish.png';
+import meat from '/public/meat.png';
+import milk from '/public/milk.png';
+import hen from '/public/hen.png';
+import veggie from '/public/veggie.png';
+import question from '/public/question.png';
 
 export default function Veggie() {
   return (
@@ -11,11 +19,33 @@ export default function Veggie() {
       </Head>
 
       <main className={styles.main}>
-        <header className={styles.recipe_header}>
-          <h1 className={styles.recipe_title}>채식을 부탁해</h1>
-          <p>채식 단계를 선택해주세요!</p>
-          <p>그에 맞는 레시피를 보여드릴게요.</p>
+        <header className={styles.header}>
+          <Image src={leafy} alt='leafy' quality={100} width={50} height={50} />
+          <h1 className={styles.veggie_title}>채식을 부탁해</h1>
         </header>
+        <section className={styles.veggie_intro}>
+          <p>채식 단계를 선택해주세요! </p>
+          <p>그에 맞는 레시피를 보여드릴게요. </p>
+          <article className={styles.veggie_select}>
+            <select>임시임</select>
+            <div id={styles.veggie_imgs}>
+              <div><Image src={veggie} alt='veggie' quality={100} width={40} height={40} /></div>
+              <div><Image src={milk} alt='milk' quality={100} width={40} height={40} /></div>
+              <div><Image src={egg} alt='egg' quality={100} width={40} height={40} /></div>
+              <div><Image src={fish} alt='fish' quality={100} width={40} height={40} /></div>
+              <div><Image src={hen} alt='hen' quality={100} width={40} height={40} /></div>
+              <div><Image src={meat} alt='meat' quality={100} width={40} height={40} /></div>
+              <div><Image src={question} alt='question' quality={100} width={25} height={25} /></div>
+            </div>
+          </article>
+          <article className={styles.veggie_list}>
+            <div className={styles.veggie_list_title}>
+              <h4>락토베지테리언</h4>
+              <h4>레시피 목록</h4>
+              <Recipe />
+            </div>
+          </article>
+        </section>
       </main>
     </div>
   )
