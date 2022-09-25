1fbd6a68ada29401b9623d95eec64e41b680daab
Hojin Jung <hojinnjung@naver.com>
2022-09-25 17:50:19 +0900

    [No issue][BE-HJ] chore: Code to crawling deals is added

---
+++ DealCrawler/dealcrawler/src/main/java/com/veggiemeal/dealcrawler/DealcrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.veggiemeal.dealcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealcrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealcrawlerApplication.class, args);
+	}
+
+}
