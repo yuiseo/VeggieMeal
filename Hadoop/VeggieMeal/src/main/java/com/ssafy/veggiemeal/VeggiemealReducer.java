5f609d9c85b567722f61fe54272a6f76952df6db
이기영 <kian6365@naver.com>
2022-09-26 16:48:13 +0900

    [S07P22C205-170][BE-기영] feat : modify reducer(add max, min value)

--- Hadoop/VeggieMeal/src/main/java/com/ssafy/veggiemeal/VeggiemealReducer.java
+++ Hadoop/VeggieMeal/src/main/java/com/ssafy/veggiemeal/VeggiemealReducer.java
@@ -13,11 +13,21 @@ public class VeggiemealReducer extends Reducer<Text, DoubleWritable, Text, Doubl
     public void reduce(Text _key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
         double sum = 0;
         double count = 0;
+        double max = Integer.MIN_VALUE;
+        double min = Integer.MAX_VALUE;
         // process values
         for (DoubleWritable val : values) {
             sum += val.get();
             count++;
+            max = Math.max(max, val.get());
+            min = Math.min(min, val.get());
         }
+        String keyStr = _key.toString();
+        keyStr += Double.toString(max);
+        keyStr += ",";
+        keyStr += Double.toString(min);
+        keyStr += ",";
+        _key = new Text(keyStr);
         context.write(_key, new DoubleWritable(sum / count));
     }
 }
