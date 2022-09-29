67459b20f8b1158b0ac693ec01367fe2bd14148f
kian6365 <kian6365@naver.com>
2022-09-29 11:28:49 +0900

    [No issue][BE-기영]feat: add swagger(func-1)

--- Back/src/main/java/com/veggiemeal/api/controller/RecipeController.java
+++ Back/src/main/java/com/veggiemeal/api/controller/RecipeController.java
@@ -2,6 +2,7 @@ package com.veggiemeal.api.controller;
 
 import com.veggiemeal.api.domain.dto.recipe.RecipeDto;
 import com.veggiemeal.api.service.recipe.RecipeService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @ApiOperation(value = "채식 카테고리를 입력받아 일치하는 레시피 데이터를 반환", response = List.class)
     @GetMapping("/")
     public ResponseEntity<List<RecipeDto>> getRecipeByCategory(@RequestParam("category") String category){
         List<RecipeDto> recipeDtoList = recipeService.getRecipeByVeg(category);
