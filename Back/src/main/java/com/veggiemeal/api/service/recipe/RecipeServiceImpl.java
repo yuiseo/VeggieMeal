73cc528d853a4ae119a6ca8f69580c64fed3fee1
kian6365 <kian6365@naver.com>
2022-09-29 15:59:59 +0900

    [No issue][BE-기영]feat: add comment

--- Back/src/main/java/com/veggiemeal/api/service/recipe/RecipeServiceImpl.java
+++ Back/src/main/java/com/veggiemeal/api/service/recipe/RecipeServiceImpl.java
@@ -31,6 +31,8 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     public List<RecipeDto> getRecipeByVeg(String category) {
         List<Recipe> recipeEntityList = recipeRepository.findAll();
+        
+        // RecipeID 값을 통해 정렬
         Collections.sort(recipeEntityList, new Comparator<Recipe>() {
             @Override
             public int compare(Recipe o1, Recipe o2) {
@@ -42,10 +44,14 @@ public class RecipeServiceImpl implements RecipeService {
 
             }
         });
+        // 채식 단계에 대한 EntityList
         List<Type> typeEntityList = typeRepository.findAll();
+        // 채식 단계에 맞는 레시피 엔티티를 추가할 List
         List<Recipe> returnList = new ArrayList<>();
 
+        // 전체 레시피에 대한 채식 단계 List를 돌면서 입력된 caterory 문자열에 해당되는 값이 1인 레시피를 returnList에 추가
         for(Type type : typeEntityList){
+            // validation 값이 true가 되면 현재 Type에 해당되는 레시피가 입력된 category에 적합하다는 의미
             boolean validation = false;
             switch (category){
                 case "vegan":
@@ -85,10 +91,12 @@ public class RecipeServiceImpl implements RecipeService {
                     break;
             }
             if(validation){
+                // List의 index는 실제 DB에 저장되어있는 레시피 ID보다 1이 크다
                 returnList.add(recipeEntityList.get(type.getTypeId() - 1));
             }
         }
 
+        // EntityList를 DtoList로 변환하여 반환
         return returnList.stream().map(entity -> RecipeDto.of(entity)).collect(Collectors.toList());
     }
 
@@ -101,16 +109,29 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public List<RecipeDto> getRecipeByIngredient(List<String> ingredient) {
+        // 전체 레시피 Entity List
         List<Recipe> recipeEntityList = recipeRepository.findAll();
+        // 반환할 레시피 Dto List
         List<RecipeDto> recipeDtoList = new ArrayList<>();
+        // 전체 재료 정보 Entity List
         List<Component> componentList = componentRepository.findAll();
+
+        /*
+         * 전체 레시피 Entity List를 돌면서 해당 레시피 ID를 가진 재료 Entity만을 DB에서 추출할 수 있지만,
+         * 500개의 레시피 Entity List를 돌면서 매번 select문을 수행하니 동작이 느려서 미리 전체 재료 Entity List를 추출하고
+         * 반복문을 통해 검사하는 방법을 사용
+         */
+        // 전체 레시피 Entity List를 돌면서 선택된 재료들(ingredient)가 모두 포함된 레시피를 찾아 recipeDtoList에 추가
         for(Recipe recipeEntity : recipeEntityList){
             int count = 0;
+            // 전체 재료 정보 Entity List를 돌면서 레시피 ID가 같고, 선택된 재료들 중 현재 재료 Entity의 이름이 포함되어있는 경우 count++
             for(Component component : componentList){
                 if(component.getRecipeId() == recipeEntity.getRecipeId() && ingredient.contains(component.getName())){
                     count++;
                 }
+                // count 값이 ingredient의 크기와 같아지면 선택된 재료들이 모두 포함되었음을 의미
                 if(count == ingredient.size()){
+                    // 반환할 DtoList에 현재 RecipeEntity를 Dto로 변환하여 추가
                     recipeDtoList.add(RecipeDto.of(recipeEntity));
                     break;
                 }
