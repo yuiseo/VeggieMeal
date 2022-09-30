aec1944faf28130a225235b885e092f959c36b32
유이서 <yoojh456@naver.com>
2022-09-30 11:44:42 +0900

    [S07P22C205-155][FE-이서]feat:API연동

--- Front/veggiemeal/pages/prices/index.tsx
+++ Front/veggiemeal/pages/prices/index.tsx
@@ -9,93 +9,86 @@ import styles from 'styles/Price.module.scss';
 import glass from '/public/glass.png';
 import SelectBox from "components/SelectBox";
 import News from "components/News";
+import { useQuery } from 'react-query';
 
-const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
-const PriceSelectBox = dynamic(() => import('components/PriceSelectBox'))
+// const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+// const PriceSelectBox = dynamic(() => import('components/PriceSelectBox'))
 // import PriceSelectBox from "components/PriceSelectBox";
+
+// const BACK_URL = 'https://j7c205.p.ssafy.io/api/deal/'
 
 export async function getServerSideProps() {
   // Fetch data from external API
   const keyword = encodeURI("물가");
   const newHeader: HeadersInit = new Headers();
-  newHeader.set("X-Naver-Client-Id", "zPEQIIbwNxiP18_gyNbN" );
+  newHeader.set("X-Naver-Client-Id", "zPEQIIbwNxiP18_gyNbN");
   newHeader.set("X-Naver-Client-Secret", "W1O4_DezL1");
   const res = await fetch(`https://openapi.naver.com/v1/search/news.json?query=${keyword}`, {
-      method:'GET',
-      headers:newHeader
+    method: 'GET',
+    headers: newHeader
   })
   const data = await res.json();
 
+  const respond = await fetch('https://j7c205.p.ssafy.io/api/deal/large', {
+    method: 'get'
+  })
+  const largeData = await respond.json()
+  // console.log(largeData)
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data, largeData } }
 }
 
-type PriceProps={
-  data:any
+type PriceProps = {
+  data: any
+  largeData: string[]
 }
 
 
-export default function Prices({data}:PriceProps) {
+export default function Prices({ data, largeData }: PriceProps,) {
   const [isSelect01, setIsSelect01] = useState<string>();
   const [isSelect02, setIsSelect02] = useState<string>();
   const [isSelect03, setIsSelect03] = useState<string>();
   const [isSelect04, setIsSelect04] = useState<string>();
-  const cat01 = ['과일', '채소', '정육', '계란', '수산/건어물', '우유/유제품', '쌀/잡곡'];
-  const cat02 = ['감자/고구마', '두부/콩나물/숙주나물', '상추/깻잎/쌈채소', '무/당근', '버섯', '배추/양배추/브로콜리', '시금치/나물'];
-  const cat03 = ['감자', '고구마'];
-  const cat04 = ['국산', '수입산'];
+  const cat01 = largeData;
+  const { data: cat02 } = useQuery(['cat02', isSelect01], async () => {
+    const res = await fetch(`https://j7c205.p.ssafy.io/api/deal/medium?large=${isSelect01}`)
+    const data = await res.json()
+    // console.log(data)
+    return data
+  })
+  const { data: cat03 } = useQuery(['cat03', isSelect02], async () => {
+    const res = await fetch(`https://j7c205.p.ssafy.io/api/deal/small?large=${isSelect01}&medium=${isSelect02}`)
+    const data = await res.json()
+    // console.log(data)
+    return data
+  })
+  const { data: cat04 } = useQuery(['cat04', isSelect03], async () => {
+    const res = await fetch(`https://j7c205.p.ssafy.io/api/deal/origin?large=${isSelect01}&medium=${isSelect02}&small=${isSelect03}`)
+    const data = await res.json()
+
+    if (data[0] == 'income') {
+      data[0] = '수입산'
+    } else if (data[0] == 'korea') {
+      data[0] = '국내산'
+    }
+    if (data[1] == 'korea') {
+      data[1] = '국내산'
+    } else if (data[1] == 'income') {
+      data[1] = '수입산'
+    }
+    return data
+  })
+
+  const { data: dealData } = useQuery(['dealData'], async () => {
+    const res = await fetch(`https://j7c205.p.ssafy.io/api/deal/?large=${isSelect01}&medium=${isSelect02}&origin=${isSelect04}&small=${isSelect03}`)
+    const data = await res.json()
+    console.log('res', res)
+    console.log('data', data)
+    return data
+  })
+
   const tableColumns = ['날짜', '최고가(원)', '최저가(원)', '평균가(원)']
-  const tableData = [
-    {
-      data_id: 1,
-      date: '9월 7일',
-      max_val: 6700,
-      min_val: 3000,
-      val: 5000,
-    },
-    {
-      data_id: 2,
-      date: '9월 8일',
-      max_val: 6700,
-      min_val: 3000,
-      val: 5000,
-    },
-    {
-      data_id: 3,
-      date: '9월 9일',
-      max_val: 6700,
-      min_val: 3000,
-      val: 5000,
-    },
-    {
-      data_id: 4,
-      date: '9월 10일',
-      max_val: 6700,
-      min_val: 3000,
-      val: 5000,
-    },
-    {
-      data_id: 5,
-      date: '9월 11일',
-      max_val: 6700,
-      min_val: 3000,
-      val: 5000,
-    },
-    {
-      data_id: 6,
-      date: '9월 12일',
-      max_val: 6700,
-      min_val: 3000,
-      val: 5000,
-    },
-    {
-      data_id: 7,
-      date: '9월 13일',
-      max_val: 6700,
-      min_val: 3000,
-      val: 5000,
-    },
-  ]
+  const tableData = dealData;
 
 
   return (
@@ -125,7 +118,7 @@ export default function Prices({data}:PriceProps) {
           </article>
 
           <section className={styles.sub_chart}>
-            <article>
+            <article className={styles.column_chart}>
               <ChartColumn />
             </article>
             <article className={styles.table_article}>
@@ -138,7 +131,7 @@ export default function Prices({data}:PriceProps) {
             <Image src="/news.png" width={50} height={50} quality={100} />
             <p className={styles.news_title}>물가 관련 뉴스</p>
           </div>
-          {data['items'].map((item:{[key:string]:string}, index:string)=><News key={index} data={item} />)}
+          {data['items'].map((item: { [key: string]: string }, index: string) => <News key={index} data={item} />)}
         </section>
       </main>
     </div >
