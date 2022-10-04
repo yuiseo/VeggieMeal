2f278de67016476f55beafbd2251de2dd5bb160c
Hojin Jung <hojinnjung@naver.com>
2022-10-05 03:57:35 +0900

    [S07P22C205-186][BE-호진] feat: Crawler SchedulerController time set completed

--- Dealcrawler/src/main/java/com/veggiemeal/dealcrawler/SchedulerController.java
+++ Dealcrawler/src/main/java/com/veggiemeal/dealcrawler/SchedulerController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 public class SchedulerController {
 
     // Everyday 00:01:00 starts crawling
-    @Scheduled(cron = "00 01 12 * * *")
+    @Scheduled(cron = "00 01 00 * * *")
     public void scheduler() {
         Runnable runnable = null;
         for (int i = 0; i < 20; i++) {
