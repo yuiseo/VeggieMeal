6727bc75c51013ac40c0dadda4c0eca13e7c37f6
junghojin <hojinnjung@naver.com>
2022-10-04 19:31:38 +0900

    [S07P22C205-190e][BE-호진] fix: 한글 encoding error

--- Back/src/main/java/com/veggiemeal/api/service/news/NewsServiceImpl.java
+++ Back/src/main/java/com/veggiemeal/api/service/news/NewsServiceImpl.java
@@ -92,18 +92,18 @@ public class NewsServiceImpl implements NewsService {
                 String splitLine[];
                 if(line.contains("title")) {
                     splitLine = line.split("\"");
-                    newsDto.setTitle(splitLine[3].replaceAll("<[^>]*>", " "));
+                    newsDto.setTitle(new String(splitLine[3].replaceAll("<[^>]*>", " ").getBytes()));
                 } else if(line.contains("originallink")) {
                     splitLine = line.split("\"");
-                    newsDto.setLink(splitLine[3].replaceAll("<[^>]*>", " "));
+                    newsDto.setLink(new String(splitLine[3].replaceAll("<[^>]*>", " ").getBytes()));
                 } else if(line.contains("pubDate")) {
                     splitLine = line.split("\"");
-                    newsDto.setPubDate(splitLine[3].replaceAll("<[^>]*>", " "));
+                    newsDto.setPubDate(new String(splitLine[3].replaceAll("<[^>]*>", " ").getBytes()));
                     newsItems.add(newsDto);
                     System.out.println(newsItems.toString());
                 } else if(line.contains("description")) {
                     splitLine = line.split("\"");
-                    newsDto.setDescription(splitLine[3].replaceAll("<[^>]*>", " "));
+                    newsDto.setDescription(new String(splitLine[3].replaceAll("<[^>]*>", " ").getBytes()));
                 }
             }
         } catch (IOException e) {
