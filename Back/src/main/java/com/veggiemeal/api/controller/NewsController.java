36b3ed9f68c0de4891457e0dd4341a90491a08c8
junghojin <hojinnjung@naver.com>
2022-10-04 17:34:27 +0900

    [S07P22C205-190e][BE-호진] feat: 물가 뉴스 API

--- Back/src/main/java/com/veggiemeal/api/controller/NewsController.java
+++ Back/src/main/java/com/veggiemeal/api/controller/NewsController.java
@@ -3,6 +3,7 @@ package com.veggiemeal.api.controller;
 import com.veggiemeal.api.domain.dto.news.NewsDto;
 import com.veggiemeal.api.service.news.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,8 @@ public class NewsController {
 
     @GetMapping
     public ResponseEntity<List<NewsDto>> getNaverNews() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "application/xml; charset=utf-8");
         return ResponseEntity.status(200).body(newsService.getNaverNews());
     }
 
