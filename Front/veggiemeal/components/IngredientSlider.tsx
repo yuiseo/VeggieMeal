cf708fc60c78dc6f0269632913216c385993b128
유이서 <yoojh456@naver.com>
2022-10-11 23:12:21 +0900

    [No issue][FE-이서]feat:슬라이더 화살표 제거

--- Front/veggiemeal/components/IngredientSlider.tsx
+++ Front/veggiemeal/components/IngredientSlider.tsx
@@ -22,7 +22,7 @@ export default function IngredientSlider({ ingredientList }: any) {
       <Swiper
         style={{ maxWidth: '85vw' }}
         spaceBetween={10}
-        slidesPerView={3}
+        slidesPerView={3.3}
         slidesPerGroup={1}
         scrollbar={{
           draggable: true,
@@ -30,10 +30,10 @@ export default function IngredientSlider({ ingredientList }: any) {
         }}
         observer={true}
         observeParents={true}
-        navigation={{
-          nextEl: '.right-swiper-button-next',
-          prevEl: '.left-swiper-button-prev',
-        }}
+        // navigation={{
+        //   nextEl: '.right-swiper-button-next',
+        //   prevEl: '.left-swiper-button-prev',
+        // }}
         // pagination={{ clickable: true }}
         breakpoints={{
           350: {
@@ -49,9 +49,6 @@ export default function IngredientSlider({ ingredientList }: any) {
           900: {
             slidesPerView: 3.3,
           },
-          1200: {
-            slidesPerView: 3,
-          }
         }}
       >
 
@@ -60,7 +57,7 @@ export default function IngredientSlider({ ingredientList }: any) {
 
 
       </Swiper>
-
+      {/* 
       <div className={styles.arrow_Container}>
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-compact-left left-swiper-button-prev" viewBox="0 0 16 16" color='#5C5ACD'>
           <path fillRule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z" />
@@ -68,7 +65,7 @@ export default function IngredientSlider({ ingredientList }: any) {
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-compact-right right-swiper-button-next" viewBox="0 0 16 16" color='#5C5ACD'>
           <path fillRule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z" />
         </svg>
-      </div>
+      </div> */}
 
 
     </div >
